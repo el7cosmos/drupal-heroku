0bd1ba9a00e5ceff2c4ae8e69a04ed919b6ddfaf
el7cosmos <el@elabee.me>
2020-08-17 06:25:47 +0700

    Keep local settings at the end of file

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -768,23 +768,8 @@ $settings['entity_update_backup'] = TRUE;
 $settings['migrate_node_migrate_type_classic'] = FALSE;
 
 /**
- * Load local development override configuration, if available.
- *
- * Create a settings.local.php file to override variables on secondary (staging,
- * development, etc.) installations of this site.
- *
- * Typical uses of settings.local.php include:
- * - Disabling caching.
- * - Disabling JavaScript/CSS compression.
- * - Rerouting outgoing emails.
- *
- * Keep this code block at the end of this file to take full effect.
+ * Database settings.
  */
-
-if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-  include $app_root . '/' . $site_path . '/settings.local.php';
-}
-
 if (isset($_ENV['DATABASE_URL'])) {
   $connection = parse_url($_ENV['DATABASE_URL']);
 
@@ -797,7 +782,7 @@ if (isset($_ENV['DATABASE_URL'])) {
 }
 
 /**
- * Redis config.
+ * Redis settings.
  */
 if (isset($_ENV['REDIS_URL'])) {
   $settings['container_yamls'][] = $app_root . '/' . $site_path . '/redis.services.yml';
@@ -851,4 +836,22 @@ if (isset($_ENV['REDIS_URL'])) {
   ];
 
   $settings['queue_default'] = 'queue.redis';
+}
+
+/**
+ * Load local development override configuration, if available.
+ *
+ * Create a settings.local.php file to override variables on secondary (staging,
+ * development, etc.) installations of this site.
+ *
+ * Typical uses of settings.local.php include:
+ * - Disabling caching.
+ * - Disabling JavaScript/CSS compression.
+ * - Rerouting outgoing emails.
+ *
+ * Keep this code block at the end of this file to take full effect.
+ */
+
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
 }
